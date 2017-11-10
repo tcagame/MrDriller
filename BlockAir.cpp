2a094a06dd3f8e168a91c96acf2d576f099493c8
og <a>
2017-11-10 09:41:28 +0900

    Airのクラスを作った

---
+++ BlockAir.cpp
@@ -0,0 +1,12 @@
+#include "BlockAir.h"
+
+
+
+BlockAir::BlockAir( int x, int y ) :
+Block( x, y, 0, 16 * 20 ) {
+}
+
+
+BlockAir::~BlockAir()
+{
+}
---
+++ BlockAir.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Block.h"
+class BlockAir:	public Block {
+public:
+	BlockAir( int x, int y );
+	virtual ~BlockAir( );
+};
+
