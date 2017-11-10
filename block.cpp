1a20dfcc62fe897fd368d49b191bf1888633bd2e
ブロック <a>
2017-11-10 10:21:09 +0900

    no message

--- block.cpp
+++ block.cpp
@@ -4,6 +4,7 @@
 const int WIDTH = 100;
 const int HEIGHT = 60;
 const int SPRITE_SIZE = 16;
+const double FALL_SPEED = 2;
 
 Block::Block( int x, int y, int tx, int ty ) :
 _x( x ),
@@ -17,6 +18,8 @@ Block::~Block( ) {
 
 void Block::update( ) {
 	//‚±‚±‚É—‰ºˆ—‚È‚Ç‚ğ‘‚­
+	act( );
+	fall( );
 }
 
 void Block::draw( int img_handle ) const {
@@ -30,3 +33,7 @@ void Block::setTx( int tx ) {
 void Block::setTy( int ty ) {
 	_ty = ty;
 };
+
+void Block::fall( ) {
+	_y += FALL_SPEED;
+}
