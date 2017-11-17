91d1de8616483abd7e8b7bba63d16bd21e88ffbf
yamada <a>
2017-11-17 14:45:49 +0900

    カメラワーク

--- Camera.cpp
+++ Camera.cpp
@@ -1,6 +1,6 @@
 #include "Camera.h"
 
-
+const int CAMERA_MOVE_SPEED = 4;
 
 Camera::Camera( std::shared_ptr< Player > player ) :
 _player( player ),
@@ -13,9 +13,17 @@ Camera::~Camera( ) {
 
 void Camera::update( ) {
 	//キャラクターの座標にあわせてy軸を変更
-	int y = _player->getY( ) - 720 / 2;
-	if ( y > _y ) {
-		_y = y;
+	int target_y = _player->getY( ) - 720 / 2;
+	if ( _y != target_y ) {
+		int vec = target_y - _y;
+		if ( abs( vec ) > CAMERA_MOVE_SPEED ) {
+			if ( vec > 0 ) {
+				vec = CAMERA_MOVE_SPEED;
+			} else {
+				vec = CAMERA_MOVE_SPEED * -1;
+			}
+		}
+		_y += vec;
 	}
 }
 
