9207b552f53459b924f038fbe956d1d502f1293f
ou <1>
2017-11-24 14:09:14 +0900

    バグ修正

--- ScenePlay.cpp
+++ ScenePlay.cpp
@@ -71,7 +71,7 @@ void ScenePlay::drawDepth( ) const {
 		if ( _player->getDepth( ) < 10 ) {
 			DrawBox( 1220 - DRAW_NUM_SIZE_X, 77, 1220, 105, RGB( 0, 0, 0 ), TRUE );
 		} else {
-			DrawBox( 1220 - ( ( int )log10( _player->getDepth( ) + 1 ) ) * DRAW_NUM_SIZE_X, 77, 1220, 105, RGB( 0, 0, 0 ), TRUE );
+			DrawBox( 1220 - ( ( int )log10( _player->getDepth( ) ) + 1 ) * DRAW_NUM_SIZE_X, 77, 1220, 105, RGB( 0, 0, 0 ), TRUE );
 		}
 		for ( int i = 0; i < 6; i++ ) {
 			DrawRectExtendGraph( DRAW_Depth_X + i * DRAW_NUM_SIZE_X, DRAW_Depth_Y, DRAW_Depth_X + ( i + 1 ) * DRAW_NUM_SIZE_X, DRAW_Depth_Y + DRAW_NUM_SIZE_Y, ( buf[ i ] - '0' ) % 5 * NUM_WIDTH, ( buf[ i ] - '0' ) / 5 * NUM_HEIGHT, NUM_WIDTH, NUM_HEIGHT, _img_num, TRUE );
@@ -108,7 +108,7 @@ void ScenePlay::drawScore( ) const {
 		if ( _player->getScore( ) < 10 ) {
 			DrawBox( 1220 - DRAW_NUM_SIZE_X, 180, 1220, 235, RGB( 0, 0, 0 ), TRUE );
 		} else {
-			DrawBox( 1220 - ( ( int )log10( _player->getScore( ) + 1 ) + 1 ) * DRAW_NUM_SIZE_X, 180, 1220, 235, RGB( 0, 0, 0 ), TRUE );
+			DrawBox( 1220 - ( ( int )log10( _player->getScore( ) ) + 1 ) * DRAW_NUM_SIZE_X, 180, 1220, 235, RGB( 0, 0, 0 ), TRUE );
 		}
 		for ( int i = 0; i < 6; i++ ) {
 			DrawRectExtendGraph( DRAW_Score_X + i * DRAW_NUM_SIZE_X, DRAW_Score_Y, DRAW_Score_X + ( i + 1 ) * DRAW_NUM_SIZE_X, DRAW_Score_Y + DRAW_NUM_SIZE_Y, ( buf[ i ] - '0' ) % 5 * NUM_WIDTH, ( buf[ i ] - '0' ) / 5 * NUM_HEIGHT, NUM_WIDTH, NUM_HEIGHT, _img_num, TRUE );
