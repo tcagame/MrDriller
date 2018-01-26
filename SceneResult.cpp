212d2d97b27df24f58033ae884b28ee360fd6991
masakisinngo <a>
2018-01-26 10:24:24 +0900

    リザルトプッシュ画像追加

--- SceneResult.cpp
+++ SceneResult.cpp
@@ -15,6 +15,10 @@ const int NUM_HEIGHT = 310;
 const int DRAW_NUM_SIZE_X = 80;
 const int DRAW_NUM_SIZE_Y = 80;
 
+const int PLEASH_PUSH_DRAW_Y = 600;
+const int PLEASE_PUSH_DRAW_WIDTH = 700;
+const int PLEASE_PUSH_DRAW_HEIGHT = 100;
+
 
 SceneResult::SceneResult( int score, int depth ):
 _score ( score ),
@@ -34,7 +38,7 @@ void SceneResult::loadGraph( ) {
 	std::shared_ptr< Graph > graph = Graph::get( );
 	graph->load( Graph::GRAPH_NUMBER );
 	graph->load( Graph::GRAPH_RESULT_BG );
-	//graph->load( Graph::GRAPH_PLEASE_PUSH_SPACE );
+	graph->load( Graph::GRAPH_PLEASE_PUSH_SPACE );
 }
 
 //-----------Update Draw-----------//
@@ -54,6 +58,7 @@ void SceneResult::draw( ) const {
 	//Graph::get( )->draw( Graph::GRAPH_PLEASE_PUSH_SPACE, FALSE, x1, y1, x2, y2 );
 	drawScore( );
 	drawDepth( );
+	drawPleasePush( );
 }
 
 void SceneResult::drawScore( ) const {
@@ -94,4 +99,15 @@ void SceneResult::drawDepth( ) const {
 		x1 += DRAW_NUM_SIZE_X;
 		x2 += DRAW_NUM_SIZE_X;
 	}
+}
+
+void SceneResult::drawPleasePush( ) const{
+	std::shared_ptr<Graph>graph=Graph::get( );
+	int x1 = ( SCREEN_WIDTH - PLEASE_PUSH_DRAW_WIDTH ) / 2;
+	int y1 = PLEASH_PUSH_DRAW_Y;
+	int x2 = x1 + PLEASE_PUSH_DRAW_WIDTH;
+	int y2 = y1 + PLEASE_PUSH_DRAW_HEIGHT;
+	SetDrawBlendMode( DX_BLENDMODE_ALPHA, ( int )( ( sin( _count * 0.06 ) + 1 ) * 64 + 156 ) );//“§–¾‰»
+	Graph::get( )->draw( Graph::GRAPH_PLEASE_PUSH_SPACE, TRUE, x1, y1, x2, y2 );
+	SetDrawBlendMode( DX_BLENDMODE_NOBLEND, 0 );
 }
